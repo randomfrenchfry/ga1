bbfb32a4a6daf37b461e4b3faddb024988455f40
Daniel Aguila <daguila658@gmail.com>
2019-09-05 13:05:21 -0500

    Update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <string>
+
+template<typename Type>
+struct Node{
+    Type data;
+    Node* next;
+};
+
+class list{
+private:
+public:
+};
+
 
 int main() {
     std::cout << "NO U" << std::endl;
